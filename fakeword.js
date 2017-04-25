0f31b992903b95bca5f06b30fe3e5f3da2c6acfa
Jeff Weston <jjweston@gmail.com>
2017-04-25 09:21:13 -0700

    Removed debug output.

--- fakeword.js
+++ fakeword.js
@@ -5,21 +5,7 @@ var fs = require( "fs" );
 
 var words = JSON.parse( fs.readFileSync( "words.json" ));
 
-console.log( "Hello world!" );
-console.log();
-
-for ( var i = 0; i < words.length; i++ )
-{
-    console.log( words[ i ] );
-}
-
-console.log();
-
 var word1 = Math.floor( Math.random() * words.length );
 var word2 = Math.floor( Math.random() * words.length );
-
-console.log( word1 );
-console.log( word2 );
-console.log();
 
 console.log( words[ word1 ] + words[ word2 ] );
