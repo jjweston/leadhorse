e837be8cf9090ea7eeafb46349788214713f97cc
Jeff Weston <jjweston@gmail.com>
2017-04-25 09:20:20 -0700

    Added logic to create fake words.

--- fakeword.js
+++ fakeword.js
@@ -1,4 +1,25 @@
 // fakeword - Create fake words by combining two randomly-selected words.
 // Copyright (C) 2017 Jeffrey J. Weston
 
-console.log( "Hello world!" )
+var fs = require( "fs" );
+
+var words = JSON.parse( fs.readFileSync( "words.json" ));
+
+console.log( "Hello world!" );
+console.log();
+
+for ( var i = 0; i < words.length; i++ )
+{
+    console.log( words[ i ] );
+}
+
+console.log();
+
+var word1 = Math.floor( Math.random() * words.length );
+var word2 = Math.floor( Math.random() * words.length );
+
+console.log( word1 );
+console.log( word2 );
+console.log();
+
+console.log( words[ word1 ] + words[ word2 ] );
